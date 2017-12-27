fc4ff6641f68958300b00344592d5e34b519cbbd
shigapovaa <shigapova.alsu@gmail.com>
2017-12-27 22:13:05 +0300

    Добавлены экраны

---
+++ RabbitSocketGame/src/sample/Controller.java
@@ -0,0 +1,47 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    private String[][] field = new String[2][2];
+
+    public void randomRabbit() {
+    }
+
+
+    public void button00(ActionEvent actionEvent) {
+
+    }
+
+    public void button01(ActionEvent actionEvent) {
+
+    }
+
+    public void button02(ActionEvent actionEvent) {
+
+    }
+
+    public void button10(ActionEvent actionEvent) {
+
+    }
+
+    public void button11(ActionEvent actionEvent) {
+
+    }
+
+    public void button12(ActionEvent actionEvent) {
+
+    }
+
+    public void button20(ActionEvent actionEvent) {
+
+    }
+
+    public void button21(ActionEvent actionEvent) {
+
+    }
+
+    public void button22(ActionEvent actionEvent) {
+
+    }
+}
---
+++ RabbitSocketGame/src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Отображение окна для входа
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //primaryStage.setTitle("Hello ");
+        //primaryStage.setScene(new Scene(root, 300, 190));
+        //primaryStage.show();
+
+        //Отображение игры
+        Parent root1 = FXMLLoader.load(getClass().getResource("Field.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root1, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
